27a66ca38d1628747fe8332877f9e346fee72e56
Carlos Gutierrez moreno <cgutierr@c2r15s6.42madrid.com>
2021-05-05 19:20:39 +0200

    Updated

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgutierr <cgutierr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/05 18:11:48 by cgutierr          #+#    #+#             */
+/*   Updated: 2021/05/05 18:31:05 by cgutierr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+
+int	ft_split_n(char const *s, char c);
+char	**ft_split(char const *s, char c);
+
+#endif
