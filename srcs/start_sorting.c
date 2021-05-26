bc594132e7d155a92bd2ff7c46bd78b12e831ba0
Carlos Gutierrez moreno <cgutierr@c2r15s6.42madrid.com>
2021-05-26 16:59:53 +0200

    26/05/2021 16:59

--- srcs/start_sorting.c
+++ srcs/start_sorting.c
@@ -6,7 +6,7 @@
 /*   By: cgutierr <cgutierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 17:03:30 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/05/26 16:54:20 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/05/26 16:59:23 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	sort_three(t_push_swap *ps)
 {
+	if (!check_order(ps->a) && ps->b == NULL)
+		exit_push_swap(ps);
 	if (ps->a->num > ps->a->next->num && ps->a->num < ps->a->next->next->num
 		&& ps->a->next->num < ps->a->next->next->num)
 		rule_sx(ps, 'a');
@@ -41,9 +43,8 @@ void	start_push_swap(int argc, char **argv, t_push_swap *ps)
 {
 	int	size;
 
-	print_stacks(ps);
 	size = ft_stacksize(ps->a);
-	if (!check_order(ps->a))
+	if (!check_order(ps->a) && ps->b == NULL)
 		exit_push_swap(ps);
 	if (size == 1)
 		exit_push_swap(ps);
@@ -55,7 +56,7 @@ void	start_push_swap(int argc, char **argv, t_push_swap *ps)
 	}
 	else if (size == 3)
 	{
-		if (!check_order(ps->a))
+		if (!check_order(ps->a) && ps->b == NULL)
 			exit_push_swap(ps);
 		sort_three(ps);
 		exit_push_swap(ps);
