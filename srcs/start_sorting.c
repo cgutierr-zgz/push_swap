b018086e7ae4af1591f14ace0e45c4680918607b
Carlos Gutierrez moreno <cgutierr@c2r15s6.42madrid.com>
2021-05-26 17:13:43 +0200

    26/05/2021 17:13

--- srcs/start_sorting.c
+++ srcs/start_sorting.c
@@ -6,7 +6,7 @@
 /*   By: cgutierr <cgutierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 17:03:30 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/05/26 16:59:23 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/05/26 17:10:30 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,21 +44,16 @@ void	start_push_swap(int argc, char **argv, t_push_swap *ps)
 	int	size;
 
 	size = ft_stacksize(ps->a);
-	if (!check_order(ps->a) && ps->b == NULL)
-		exit_push_swap(ps);
-	if (size == 1)
+	if (!check_order(ps->a))
 		exit_push_swap(ps);
 	else if (size == 2)
 	{
 		if (ps->a->num > ps->a->next->num)
 			rule_sx(ps, 'a');
-		exit_push_swap(ps);
 	}
 	else if (size == 3)
-	{
-		if (!check_order(ps->a) && ps->b == NULL)
-			exit_push_swap(ps);
 		sort_three(ps);
-		exit_push_swap(ps);
-	}
+	// pa 4 poner el mas peque arriba y moverlo a b hacer 3 en a y meter de b a ->>a
+	// pa5 poner el mas peque arriba y ell segundo mas peque abajo y moverlo a b hacer 3 en a y meter de b a ->>a x 2
+	exit_push_swap(ps);
 }
